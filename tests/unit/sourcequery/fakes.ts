d3bc10fc97da51f794020438cf9abc32693b4fb5
oletizi <oletizi@mac.com>
2026-07-17 12:47:18 -0700

    test(014 T003): scaffold sourcequery test dirs + shared fakes placeholder

---
+++ tests/unit/sourcequery/fakes.ts
@@ -0,0 +1,13 @@
+/**
+ * Shared test fixtures for sourcequery module.
+ *
+ * This module will hold test fakes and fixtures including:
+ * - FakeBrowserSession (defined in T005)
+ * - FakeTailscaleRunner (defined in T006)
+ * - Fake clock/sleep utilities (defined in T007)
+ *
+ * These fakes are populated incrementally as their corresponding
+ * interfaces and implementations are developed in later tasks.
+ */
+
+export {};
